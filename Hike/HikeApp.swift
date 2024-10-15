b6daa5fd9f4c85bd82edae112dd8d097b8a6415f
Metehan Özden <metehanozden.bil@gmail.com>
2024-10-15 16:20:17 +0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Metehan Özden on 15.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
